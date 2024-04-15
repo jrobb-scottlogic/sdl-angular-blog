80a3839d47e30b9f5b3cf65ee0389fd91fc5296e
James Robb <125265795+jrobb-scottlogic@users.noreply.github.com>
2024-04-15 16:48:07 +0100

    homepage css changes

--- src/app/post-thumbnail/post-thumbnail.component.ts
+++ src/app/post-thumbnail/post-thumbnail.component.ts
@@ -1,10 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { PostSkeleton } from '../post-skeleton';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-post-thumbnail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './post-thumbnail.component.html',
   styleUrl: './post-thumbnail.component.css',
 })

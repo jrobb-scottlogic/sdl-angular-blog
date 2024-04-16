e631d8acb1daa4375d674c51be98889d61384926
James Robb <125265795+jrobb-scottlogic@users.noreply.github.com>
2024-04-16 15:04:59 +0100

    added tags

--- src/app/post-thumbnail/post-thumbnail.component.ts
+++ src/app/post-thumbnail/post-thumbnail.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { PostSkeleton } from '../post-skeleton';
 import { CommonModule } from '@angular/common';
+import { TagComponent } from '../tag/tag.component';
 @Component({
   selector: 'app-post-thumbnail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TagComponent],
   templateUrl: './post-thumbnail.component.html',
   styleUrl: './post-thumbnail.component.css',
 })

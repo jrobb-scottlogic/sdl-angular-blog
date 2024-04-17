450431928f1216ee0832934dc0ccf328028c01fe
Nathanael Brown <nbrown@scottlogic.com>
2024-04-17 10:37:40 +0100

    Routing fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { PostComponent } from './post/post.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'article/:id', component: PostComponent},
+  {path: '', component: HomepageComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class app{}
+
--- src/app/post-thumbnail/post-thumbnail.component.ts
+++ src/app/post-thumbnail/post-thumbnail.component.ts
@@ -1,14 +1,23 @@
-import { Component, Input } from '@angular/core';
-import { PostSkeleton } from '../post-skeleton';
 import { CommonModule } from '@angular/common';
+import { Component, Input, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TagComponent } from '../tag/tag.component';
+import { PostSkeleton } from './../post-skeleton';
+import { PostService } from './../post.service';
 @Component({
   selector: 'app-post-thumbnail',
   standalone: true,
-  imports: [CommonModule, TagComponent],
+  imports: [CommonModule, TagComponent, RouterLink],
   templateUrl: './post-thumbnail.component.html',
   styleUrl: './post-thumbnail.component.css',
 })
 export class PostThumbnailComponent {
-  @Input() postSkeleton!: PostSkeleton;
+  @Input() postSkeleton: PostSkeleton | undefined;
+
+  postService: PostService = inject(PostService);
+
+  @Input()
+  set id(id: number) {
+    this.postSkeleton = this.postService.getPostById(id);
+  }
 }

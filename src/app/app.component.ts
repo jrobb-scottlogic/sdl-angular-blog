6417a94b202bafcd64947b9a35df525ff0bfc3e4
Nathanael Brown <nbrown@scottlogic.com>
2024-04-17 14:36:14 +0100

    Nav bar styled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomepageComponent],
+  imports: [RouterOutlet, HomepageComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })

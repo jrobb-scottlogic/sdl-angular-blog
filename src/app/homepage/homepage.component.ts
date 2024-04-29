8b3fd5ba6d8ac3bfa9a61155e7bad79395b9a455
James Robb <125265795+jrobb-scottlogic@users.noreply.github.com>
2024-04-29 14:35:41 +0100

    skeleton for featured story still needs css

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RecentPostsComponent } from '../recent-posts/recent-posts.component';
+import { FeaturedStoryComponent } from '../featured-story/featured-story.component';
+import { PostService } from '../post.service';
+import { PostSkeleton } from '../post-skeleton';
+
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [CommonModule, RecentPostsComponent],
+  imports: [CommonModule, RecentPostsComponent, FeaturedStoryComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css',
 })
-export class HomepageComponent {}
+export class HomepageComponent {
+  featured: PostSkeleton | undefined;
+  postService: PostService = inject(PostService);
+
+  constructor() {
+    this.featured = this.postService.getFeaturedPost();
+  }
+}
